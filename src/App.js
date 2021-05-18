13a78434eb68bea921bae39198fcfd2ce19ae9a8
Andres Eloy <30258079@correo.unimet.edu.ve>
2021-05-18 19:59:13 -0400

    Unified the Forms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { UsuarioRoute } from "./Componentes/UsuarioRoute";
 import { Principal } from "./Vistas/Principal";
 import { Login } from "./Vistas/Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Test from "./Vistas/Test";
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Principal} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/test" component={Test} />
       </Switch>
     </Router>
   );
--- src/Vistas/Test.jsx
+++ src/Vistas/Test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function App() {
+function Test() {
   return (
     <div className="form">
       <form action="">
@@ -45,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+export default Test;
