3344fa2368f8e2d2203d84df669daf68698b72ae
Manuel Gómez <gomezmanuel1309@gmail.com>
2021-05-18 20:07:24 -0400

    hotfix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { UsuarioRoute } from "./Componentes/UsuarioRoute";
 import { Principal } from "./Vistas/Principal";
 import { Login } from "./Vistas/Login";
