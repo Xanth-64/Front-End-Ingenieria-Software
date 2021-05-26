e1393cb222fe402845eb61226314008bcfcc9560
Andres Eloy <30258079@correo.unimet.edu.ve>
2021-05-26 02:08:26 -0400

    Publicidad Done

---
+++ src/Componentes/Publicidad.jsx
@@ -0,0 +1,23 @@
+export const Publicidad = (props) => {
+  return (
+    <div className="publicidad-card-container">
+      <div className="publicidad-card-Title">
+        <h1>Ofertas de la Semana</h1>
+      </div>
+      <div className="publicidad-card-productContainer">
+        {props.publicityProducts.map((element, index) => {
+          return (
+            <div
+              className="publicidad-card-product"
+              style={{ backgroundImage: "url(" + element.fotos[0] + ")" }}
+              key={index}
+            >
+              <div className="publicidad-product-name">{element.nombre}</div>
+              <div className="publicidad-product-price">{element.precio}$</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
