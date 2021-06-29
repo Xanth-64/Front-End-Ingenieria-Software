80dec075cd60065cd47cf078728883c4fa9e64a6
MaryBeltran <m.beltran@correo.unimet.edu.ve>
2021-06-29 17:46:28 -0400

    Progreso vista y arreglo Sign Out

---
+++ src/Vistas/PerfilPropio.jsx
@@ -0,0 +1,6 @@
+import {PerfilUsuario} from "../Componentes/PerfilUsuario"
+
+const PerfilPropio = () => { 
+    return (<> <PerfilUsuario dataUsuario = {{id_usuario: 999, nombre: "Mary", apellido: "DS", email: "marybbds98@pruebita.com", telefono: "04141330078", imagen_url: "helloooo Andresito"}}/> </>)
+
+ }
